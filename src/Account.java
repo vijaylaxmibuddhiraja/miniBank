f84c7a7e615a992cb07ae3f2f35ef34830f9cce3
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2025-01-15 20:31:51 +0100

    attributes declared,constructor initialized, method also done

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,37 @@
-public class Account {
+public abstract class Account {
+    private static long accCounter = 1000000000;
+    protected String accountNumber;
+    protected double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+    }
+
+    public String getAccountNumber() {
+        return String.format("%010d", accCounter++);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }else{
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }else{
+            System.out.println("Insufficient amount");
+        }
+    }
+
+public abstract String getAccountType();
+
 }
