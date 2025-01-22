936b8c8a9a105f38f2b0ef7adb18b553c7dd7b61
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2025-01-22 20:55:58 +0100

    savings account also working now

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SalaryAccount salAccount = new SalaryAccount("Nit1024",50000);
-
+       SalaryAccount salAccount = new SalaryAccount("Nit1024",50000);
         salAccount.getAccountDetails();
 
         salAccount.deposit(5000);
         salAccount.getAccountDetails();
 
-        salAccount.getAccountDetails();
         salAccount.withdraw(950);
         salAccount.getAccountDetails();
+
+        SavingsAccount savAccount = new SavingsAccount("Nit1088",250000);
+        savAccount.getAccountDetails();
+
+        savAccount.deposit(20000);
+        savAccount.getAccountDetails();
+
+        savAccount.withdraw(10000);
+        savAccount.getAccountDetails();
+
     }
 }
